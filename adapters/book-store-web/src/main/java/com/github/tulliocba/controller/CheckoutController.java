b736b5e7df7699e843af2d873f86e010fc58301b
Tulio Gabriel <tullio_cba@hotmail.com>
2020-02-05 10:18:11 -0300

    Changed CheckoutCommand class

--- adapters/book-store-web/src/main/java/com/github/tulliocba/controller/CheckoutController.java
+++ adapters/book-store-web/src/main/java/com/github/tulliocba/controller/CheckoutController.java
@@ -5,8 +5,10 @@ import com.github.tulliocba.bookstore.store.application.port.in.CheckoutUseCase.
 import com.github.tulliocba.bookstore.store.application.service.ItemUnavailableException;
 import com.github.tulliocba.bookstore.store.application.service.PromotionCodeNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -17,6 +19,7 @@ public class CheckoutController {
     private final CheckoutUseCase checkoutUseCase;
 
     @PostMapping("/book-store/checkout")
+    @ResponseStatus(HttpStatus.CREATED)
     void checkout(@RequestBody @Valid CheckoutCommand command) throws ItemUnavailableException, PromotionCodeNotFoundException {
         checkoutUseCase.checkout(command);
     }
--- book-store-application/src/main/java/com/github/tulliocba/bookstore/store/application/port/in/CheckoutUseCase.java
+++ book-store-application/src/main/java/com/github/tulliocba/bookstore/store/application/port/in/CheckoutUseCase.java
@@ -2,6 +2,9 @@ package com.github.tulliocba.bookstore.store.application.port.in;
 
 import com.github.tulliocba.bookstore.store.application.service.ItemUnavailableException;
 import com.github.tulliocba.bookstore.store.application.service.PromotionCodeNotFoundException;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import javax.validation.constraints.NotEmpty;
@@ -13,6 +16,8 @@ public interface CheckoutUseCase {
     void checkout(CheckoutCommand checkoutCommand) throws ItemUnavailableException, PromotionCodeNotFoundException;
 
     @Value
+    @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+    @AllArgsConstructor
     class CheckoutCommand {
 
         @NotNull
@@ -25,6 +30,8 @@ public interface CheckoutUseCase {
     }
 
     @Value
+    @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+    @AllArgsConstructor
     class Item {
         @NotNull
         private final String itemId;
